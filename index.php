99ead07b080ad2fe2a4514d81e639bb824f27b04
muckacid <moraga.inf@gmail.com>
2019-05-09 22:07:27 -0400

    modificacion en la validacion

--- index.php
+++ index.php
@@ -1,36 +1,28 @@
-
 <?php   
     require "clases/EscalaNota.php";
 
+    $notaMinima = isset($_GET["notaMinima"]);
+    $notaMaxima = isset($_GET["notaMaxima"]);
+    $punMax = isset($_GET["puntajeMaximo"]);
+    $porEx = isset($_GET["porcentajeExigencia"]);
+    $notaAprovacion = isset($_GET["notaAprovacion"]);
+    $incremento = isset($_GET["incremento"]);
+    $puntaje = isset($_GET["puntaje"]);
 
-    $notaMinima = $_GET["notaMinima"];
-    $notaMaxima = $_GET["notaMaxima"];
-    $punMax = $_GET["puntajeMaximo"];
-    $porEx = $_GET["porcentajeExigencia"];
-    $notaAprovacion = $_GET["notaAprovacion"];
-    $incremento = $_GET["incremento"];
-    
+    if($notaMinima && $notaMaxima && $punMax && $porEx && $notaAprovacion && $incremento){
+        $escala = new EscalaNota($_GET["notaMinima"],$_GET["notaMaxima"],$_GET["puntajeMaximo"],$_GET["porcentajeExigencia"],$_GET["notaAprovacion"],$_GET["incremento"]);
+    }
 
-    //http://localhost/EscalaNotas/?notaMinima=1&notaMaxima=7&puntajeMaximo=100&porcentajeExigencia=60&notaAprovacion=4&incremento=1
-    //echo "notaMinima:$notaMinima,notaMaxima:$notaMaxima,puntajeMaximo:$punMax,porcentajeExigencia:$porEx,notaAprovacion:$notaAprovacion,incremento:$incremento";
-
-    $escala = new EscalaNota($notaMinima,  $notaMaxima, $punMax, $porEx, $notaAprovacion, $incremento); 
-
-    if(isset($_GET["puntaje"])){
+    if($notaMinima && $notaMaxima && $punMax && $porEx && $notaAprovacion && $incremento && $puntaje){
         $puntaje = $_GET["puntaje"];
         $nota = $escala->calcularNota($puntaje);
         $data = array();
         $data[] = array( "$puntaje" => "$nota");
         echo json_encode($data);
     }
-    else{
+    elseif($notaMinima && $notaMaxima && $punMax && $porEx && $notaAprovacion && $incremento){
         $es = $escala->obtenerEscala();
         echo json_encode($es);
     }
-
-    //Porcentaje de aprovacion , puntaje maximo
-    //$nota = new Nota($porcentajeDeExigencia,$puntajeMaximo);
-
-
 
 ?>
